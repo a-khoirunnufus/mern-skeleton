e6a38ce5f55896e68cf2687d84d8aef36368211c
Ahmad Khoirunnufus <a.khoirunnufus@gmail.com>
2021-11-13 21:39:35 +0700

    setting up for react development

--- server/express.js
+++ server/express.js
@@ -4,12 +4,20 @@ import cookieParser from "cookie-parser"
 import compress from "compression"
 import cors from "cors"
 import helmet from "helmet"
+import path from 'path'
 
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
 import Template from './../template'
+import devBundle from "./devBundle";
+
+const CURRENT_WORKING_DIR = process.cwd()
 
 const app = express()
+
+devBundle.compile(app)
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
